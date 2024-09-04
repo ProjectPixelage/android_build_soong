e42db04cc765550eb491efad843c32ad4824086a
AdarshGrewal <adarshgrewal@gmail.com>
2024-09-04 13:38:39 +0530

    Add PixelageDeviceName and PixelageDevice variables

--- android/variable.go
+++ android/variable.go
@@ -232,6 +232,7 @@ type ProductVariables struct {
 	Platform_version_known_codenames       *string  `json:",omitempty"`
 
 	DeviceName                            *string  `json:",omitempty"`
+	PixelageDeviceName                     *string  `json:",omitempty"`
 	DeviceProduct                         *string  `json:",omitempty"`
 	DeviceArch                            *string  `json:",omitempty"`
 	DeviceArchVariant                     *string  `json:",omitempty"`
@@ -527,6 +528,7 @@ type ProductVariables struct {
 	BoardUseVbmetaDigestInFingerprint *bool `json:",omitempty"`
 
 	OemProperties []string `json:",omitempty"`
+	PixelageDevice []string `json:",omitempty"`
 
 	ArtTargetIncludeDebugBuild *bool `json:",omitempty"`
 
